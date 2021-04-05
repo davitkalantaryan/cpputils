b6a7bf435672d627d7c12f5b9216ad1a2b7b51de
Davit Kalantaryan <davit.kalantar@desy.de>
2021-04-05 13:44:44 +0200

    some tests on windows

--- scripts/.cicd/windows_test_pull_to_master_build.bat
+++ scripts/.cicd/windows_test_pull_to_master_build.bat
@@ -19,8 +19,8 @@ msbuild -t:restore -p:RestorePackagesConfig=true
 if not "!ERRORLEVEL!"=="0" (exit /b !ERRORLEVEL!)
 
 rem cd "%repositoryRoot%workspaces\cpputils_vs"
-call msbuild "%repositoryRoot%workspaces\cpputils_vs\cpputils.sln" /t:%ActionConfirm% /p:Configuration=%Configuration% /p:Platform=%Platform% -m:2
-if not "!ERRORLEVEL!"=="0" (exit /b !ERRORLEVEL!)
+rem call msbuild "%repositoryRoot%workspaces\cpputils_vs\cpputils.sln" /t:%ActionConfirm% /p:Configuration=%Configuration% /p:Platform=%Platform% -m:2
+rem if not "!ERRORLEVEL!"=="0" (exit /b !ERRORLEVEL!)
 
 rem do we need to compile makefile projects, then init env and do
 rem if not "!VSCMD_ARG_HOST_ARCH!"=="!Platform!" (
