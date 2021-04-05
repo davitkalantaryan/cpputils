9deb066a0688596b55a5b47a20de22c962b092b0
Davit Kalantaryan <davit.kalantaryan@desy.de>
2021-04-05 13:19:52 +0200

    fixed failing part for linux

--- scripts/.cicd/unix_prebuild.sh
+++ scripts/.cicd/unix_prebuild.sh
@@ -2,6 +2,15 @@
 
 # script to prepare developer host, to work with the code on this repo
 
+# https://intoli.com/blog/exit-on-errors-in-bash-scripts/
+# exit when any command fails
+set -e
+
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+#trap 'echo "\"${last_command}\" command finished with exit code $?."' EXIT
+
 scriptFileFullPath=`readlink -f ${0}`
 scriptDirectory=`dirname ${scriptFileFullPath}`
 cd ${scriptDirectory}/../..
