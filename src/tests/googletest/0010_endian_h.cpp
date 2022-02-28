6c766a933ae242dc4dc5b3b240499e0768deea75
Davit Kalantaryan <davit.kalantar@desy.de>
2022-02-28 10:46:21 +0100

    modified endian header

---
+++ src/tests/googletest/0010_endian_h.cpp
@@ -0,0 +1,22 @@
+//
+// file:			0010_endian_h.cpp
+// created on:		2022 Feb 28
+//
+
+#include <cpputils_internal_header.h>
+#include "gtest/gtest.h"
+#include <cpputils/endian.h>
+
+
+TEST(f_0010_endian_h, t0001_htobe32)
+{
+	//cpputils::hash::Hash<int, int> aHash;
+	//
+	//cpputils::hash::Hash<int, int>::iterator iter = aHash.AddEntryEvenIfExistsC(::std::pair<int,int>(1,1));
+	//aHash.AddEntryEvenIfExistsC(::std::pair<int,int>(1,1));
+	//
+	//ASSERT_EQ(aHash.size(), size_t(2));
+	//ASSERT_EQ(aHash.findEntry(1)->second,1);
+	//aHash.RemoveEntryRaw(iter);
+	//ASSERT_EQ(aHash.size(), size_t(1));
+}
