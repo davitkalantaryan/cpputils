8eb5c25e7053ad2792369c5aeaf54b08e32ba509
DAVIT Kalantaryan <Davit Kalantaryan>
2022-09-25 15:07:18 +0200

    fixed some google test runtime issue

--- src/tests/googletest/0012_hash_dllhash.cpp
+++ src/tests/googletest/0012_hash_dllhash.cpp
@@ -17,8 +17,10 @@ TEST(f_0012_hash_dllhash, t0001)
 	::cpputils::hash::DllHash<int, int> aHash;
 
 	aHash.AddEntryIfNotExistC(::std::pair<int,int>(1,1));
+	ASSERT_EQ(aHash.size(), size_t(1));
 	aHash.AddEntryIfNotExistC(::std::pair<int, int>(1, 1));
-
+	ASSERT_EQ(aHash.size(), size_t(1));
+	aHash.AddEntryEvenIfExistsC(::std::pair<int, int>(1, 1));
 	ASSERT_EQ(aHash.size(), size_t(2));
 	ASSERT_EQ(aHash.find(1)->second,1);
 	aHash.RemoveEntryRaw(aHash.begin());
