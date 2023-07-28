572f34436d13f7f29246277c773e1809cf497bcd
Davit Kalantaryan <davit.kalantar@desy.de>
2023-07-28 02:31:53 +0200

    fixed wrng windows compilation

--- scripts/windows_build_all.bat
+++ scripts/windows_build_all.bat
@@ -60,14 +60,14 @@ for %%p in (%PlatformTarget%) do (
 	for %%c in (%Configuration%) do (
 		echo "!!!!!!!!!!!! !!!!!!!!!!!! compiling for configuration %%c"
 		rem call msbuild "%repositoryRoot%workspaces\cpputils_all_vs\cpputils_all.sln" /t:!ActionConfirm! /p:Configuration=%%c /p:Platform=%%p
-		msbuild "%repositoryRoot%workspaces\cpputils_all_vs\cpputils_all2.sln" /t:!ActionConfirm! /p:Configuration=%%c /p:Platform=%%p
+		call msbuild "%repositoryRoot%workspaces\cpputils_all_vs\cpputils_all.sln" /t:!ActionConfirm! /p:Configuration=%%c /p:Platform=%%p
 		if not "!ERRORLEVEL!"=="0" (exit /b !ERRORLEVEL!)
 		call nmake -f cpputils_unit_test.windows.Makefile /e Platform=%%p /e Configuration=%%c
 		if not "!ERRORLEVEL!"=="0" (exit /b !ERRORLEVEL!)
 	)
 )
 
-exit /b !ERRORLEVEL!
+exit /b 0
 
 :parse_argument
 	set isNextArgPlatform=true
