20875bc38fd12301a23e74d5c164bf9153ed10b2
Davit Kalantaryan <davit.kalantaryan@desy.de>
2026-03-25 08:23:19 +0100

    adedd some new api to demonstrate how to ask hashes size

--- src/tests/unit_test/0009_new_hash.cpp
+++ src/tests/unit_test/0009_new_hash.cpp
@@ -20,9 +20,11 @@ TEST(f_0009_new_hash, t0001_Hash)
 
 	ASSERT_EQ((aHash.find<int,int>(1)->data),1);
 	
+    ASSERT_EQ(aHash.getHash()->count, size_t(1));
     aHash.RemoveEx(iter);
     iter = aHash.find<int, int>(1);
 	ASSERT_EQ(iter, CPPUTILS_NULL);
+    ASSERT_EQ(aHash.getHash()->count, size_t(0));
 }
 
 
@@ -38,6 +40,7 @@ TEST(f_0009_new_hash, t0002_LHash)
     ASSERT_EQ((aHash.find<int, int>(1)->data), 1);
     aHash.RemoveEx(iter);
     ASSERT_EQ(aHash.count<int>(), size_t(1));
+    ASSERT_EQ(aHash.getHash()->count, size_t(1));
 }
 
 
