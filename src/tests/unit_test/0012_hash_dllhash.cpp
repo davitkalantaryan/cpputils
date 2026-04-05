1abed61e631efe6a76c23ea2e6db9d0d052abab9
Davit Kalantaryan <davit.kalantaryan@desy.de>
2026-04-05 23:24:07 +0200

    fixed hash related unit tests

--- src/tests/unit_test/0012_hash_dllhash.cpp
+++ src/tests/unit_test/0012_hash_dllhash.cpp
@@ -26,12 +26,10 @@
 #ifdef TRY_LISTHASH
 
 typedef ::cpputils::hash::ListHash  MapToTest;
-#define CPPUTILS_TTCHI              CPPUTILS_LHCHI
 
 #else
 
 typedef ::cpputils::hash::PureHash  MapToTest;
-#define CPPUTILS_TTCHI              CPPUTILS_PHCHI
 
 #endif
 
@@ -68,7 +66,7 @@ TEST(f_0012_hash_dllhash, t0001)
     }
 
     // CPPUTILS_CHI
-    iter = aMap.findEx<int, CPPUTILS_TTCHI(int) >(1, &unHash);
+    iter = aMap.findEx<int, CPPUTILS_HASH_CHI(int) >(1, &unHash);
     ::std::cout << "iter_06: " << iter << ::std::endl;
 
     iter = aMap.AddEvenIfExist<int, int>(1, 1);
